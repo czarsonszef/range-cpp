3a4d5e4acd65d2761e30593f11e34d8a59afdec4
czarsonszef <czarson2115@wp.pl>
2022-08-01 15:30:52 +0200

    final

--- range.hpp
+++ range.hpp
@@ -1,203 +1,94 @@
 #ifndef RANGE_HPP_INCLUDED
 #define RANGE_HPP_INCLUDED
 
-
 #include <type_traits>
 
+namespace rg {
 
-namespace rg
-{
+    template <class T, class U, class W = int>
+    class range {
 
+        static_assert(std::is_arithmetic_v<T> &&
+                      std::is_arithmetic_v<U> &&
+                      std::is_arithmetic_v<W>);
 
-        namespace detail 
-        {
-                
+        struct end_empty_tag {};
 
-                template
-                <
-                        class,
-                        class,
-                        class
-                >
-                class range_begin_type;
-                
+    public:
 
-                struct range_end_type
-                {
-
-                };
-
-
-                template <class T>
-                constexpr auto less(T a, T b) noexcept
-                {
-                        return a < b;
-                }
-
-
-                template <class T>
-                constexpr auto greater(T a, T b) noexcept
-                {
-                        return a > b;
-                }
-
-                
-        } /* namespace detail */
-
-
-        template
-        <
-                class T,
-                class U,
-                class W = int
-        >
-        class range
-        {
-
-
-                static_assert
-                (
-                        std::is_arithmetic_v<T> &&
-                        std::is_arithmetic_v<U> &&
-                        std::is_arithmetic_v<W>
-                );
-
+        class __range_begin_type {
 
         public:
 
+            constexpr __range_begin_type(range<T, U, W>& rng) noexcept
+                : m_rng{rng}
+            {
 
-                using InnerType = std::common_type_t<T, U, W>;
+            }
 
+            constexpr auto operator++() noexcept -> decltype(auto) {
+                m_rng.m_n += m_rng.m_step;
+                return *this;
+            }
 
-                friend class detail::range_begin_type<T, U, W>;
+            constexpr auto operator!=(end_empty_tag) const noexcept {
+                return m_rng.m_cmp(m_rng.m_n, m_rng.m_to);
+            }
 
-
-                range() = delete;
-
-
-                constexpr range(T from, U to, W step = 1) noexcept
-                        : m_n
-                          {
-                                  static_cast<InnerType>
-                                  (
-                                        from
-                                  )
-                          }
-
-                        , m_to
-                          {
-                                  static_cast<InnerType>
-                                  (
-                                        to
-                                  )
-                          }
-
-                        , m_step
-                          {
-                                  static_cast<InnerType>
-                                  (
-                                        step
-                                  )
-                          }
-
-                        , m_cmp
-                          {
-                                  from < to ?
-                                        &detail::less<InnerType>
-                                        :
-                                        &detail::greater<InnerType>
-                          }
-
-                {
-
-                        if ((m_n < m_to && m_step < 0) || (m_n > m_to && m_step > 0)) // if this check passes it means the range is incorrect (for example 'range(5, 20, -5)')
-                        {
-                                m_n = m_to;     // makes detail::range_begin_type::operator!=() return false, the for loop won't be entered
-                        }
-
-                }
-
-
-                constexpr auto begin() noexcept -> detail::range_begin_type<T, U, W>
-                {
-                        return *this;
-                }
-
-
-                constexpr auto end() const noexcept -> detail::range_end_type
-                {
-                        return
-                                {
-
-                                };
-                }
-        
+            constexpr auto operator*() const noexcept {
+                return m_rng.m_n;
+            }
 
         private:
 
-
-                InnerType m_n,
-                          m_to,
-                          m_step;
-
-
-                bool (*m_cmp)(InnerType, InnerType);
-
+            range<T, U, W>& m_rng;
 
         };
 
+        friend class __range_begin_type;
 
-        template
-        <
-                class T,
-                class U,
-                class W
-        >
-        class detail::range_begin_type
+        using inner_type = std::common_type_t<T, U, W>;
+
+    private:
+
+        static constexpr bool less(inner_type lhs, inner_type rhs) noexcept {
+            return lhs < rhs;
+        }
+
+        static constexpr bool greater(inner_type lhs, inner_type rhs) noexcept {
+            return lhs > rhs;
+        }
+
+    public:
+
+        constexpr range(T from, U to, W step = 1) noexcept
+            : m_n    { static_cast<inner_type>(from) }
+            , m_to   { static_cast<inner_type>( to ) }
+            , m_step { static_cast<inner_type>(step) }
+            , m_cmp  { from < to ? &less : &greater  }
         {
-        public:
+            if ((m_n < m_to && m_step < 0) || (m_n > m_to && m_step > 0)) // if this check passes it means the range is incorrect (for example 'range(5, 20, -5)')
+            {
+                m_n = m_to; // makes detail::range_begin_type::operator!=() return false, the for loop won't be entered
+            }
+        }
 
+        constexpr auto begin() noexcept -> __range_begin_type {
+            return *this;
+        }
 
-                range_begin_type() = delete;
+        constexpr auto end() const noexcept -> end_empty_tag {
+            return {};
+        }
 
+    private:
 
-                constexpr range_begin_type(range<T, U, W>& rng) noexcept
-                        : m_rng
-                          {
-                                  rng
-                          }
-                {
+        inner_type m_n;
+        inner_type m_to;
+        inner_type m_step;
+        bool (*m_cmp)(inner_type, inner_type);
 
-                }
-
-
-                constexpr auto operator++() noexcept -> decltype(auto)
-                {
-                        m_rng.m_n += m_rng.m_step;
-
-                        return *this;
-                }
-
-
-                constexpr auto operator!=(detail::range_end_type const&) const noexcept
-                {
-                        return m_rng.m_cmp(m_rng.m_n, m_rng.m_to);
-                }
-
-
-                constexpr auto operator*() const noexcept
-                {
-                        return m_rng.m_n;
-                }
-
-
-        private:
-
-
-                range<T, U, W>& m_rng;
-
-
-        };
+    };
 
 
 } /* namespace rg */
